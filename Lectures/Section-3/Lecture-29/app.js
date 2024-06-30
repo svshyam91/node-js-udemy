ba7118bdbad901261333ae78e6d204ef3f95761b
svshyam91 <svshyam97@gmail.com>
2024-06-30 19:51:38 +0530

    Lecture:30 Sending Response

--- Lectures/Section-3/Lecture-29/app.js
+++ Lectures/Section-3/Lecture-29/app.js
@@ -2,6 +2,11 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method, req.headers);
+    res.write('<html>');
+    res.write('<head><title>My first page</title></head>');
+    res.write('<body>Hello World</body>');
+    res.write('</html>');
+    res.end();
 });
 
 server.listen(3000);

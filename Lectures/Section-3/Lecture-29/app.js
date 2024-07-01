c302865a2c988c3f0205d13a9c955e01dcc336ff
svshyam91 <svshyam97@gmail.com>
2024-07-02 00:31:10 +0530

    Lecture:32 Routing Requests

--- Lectures/Section-3/Lecture-29/app.js
+++ Lectures/Section-3/Lecture-29/app.js
@@ -2,6 +2,16 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method, req.headers);
+    const url = req.url;
+    if (url === '/') {
+        res.write('<html>');
+        res.write('<head><title>My first Page</title><head>');
+        res.write(
+            '<body><form method="POST" action="/message"><input type="text" name="message"><button type="submit">Submit</button></form></body>'
+        );
+        res.write('</html>');
+        return res.end();
+    }
     res.write('<html>');
     res.write('<head><title>My first page</title></head>');
     res.write('<body>Hello World</body>');

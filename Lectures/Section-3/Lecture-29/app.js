31872e18fbe3f57d257b29ca771e0d2c8a3c8ce5
svshyam91 <svshyam97@gmail.com>
2024-07-07 21:03:14 +0530

    Lecture: 35 Parsing Request Bodies

--- Lectures/Section-3/Lecture-29/app.js
+++ Lectures/Section-3/Lecture-29/app.js
@@ -2,7 +2,6 @@ const http = require('http');
 const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
     const url = req.url;
     const method = req.method;
     if (url === '/') {
@@ -15,7 +14,16 @@ const server = http.createServer((req, res) => {
         return res.end();
     }
     if (url === '/message' && method === 'POST') {
-        fs.writeFileSync('message.txt', 'DUMMY');
+        const body = [];
+        req.on('data', (chunk) => {
+            body.push(chunk);
+        });
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            console.log(parsedBody);
+            const message = parsedBody.split('=')[1];
+            fs.writeFileSync('message.txt', message);
+        });
         res.statusCode = 302;
         res.setHeader('Location', '/');
         return res.end();

9f2fab0c57681ce585d0c8b33b9e5babbd2ce271
svshyam91 <svshyam97@gmail.com>
2024-07-07 21:12:16 +0530

    Lecture: 36 Understanding Event Driven Code Execution

--- Lectures/Section-3/Lecture-29/app.js
+++ Lectures/Section-3/Lecture-29/app.js
@@ -18,15 +18,15 @@ const server = http.createServer((req, res) => {
         req.on('data', (chunk) => {
             body.push(chunk);
         });
-        req.on('end', () => {
+        return req.on('end', () => {
             const parsedBody = Buffer.concat(body).toString();
             console.log(parsedBody);
             const message = parsedBody.split('=')[1];
             fs.writeFileSync('message.txt', message);
+            res.statusCode = 302;
+            res.setHeader('Location', '/');
+            return res.end();
         });
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end();
     }
     res.write('<html>');
     res.write('<head><title>My first page</title></head>');

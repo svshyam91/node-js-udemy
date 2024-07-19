f27ab92c79afc17f8b33f4320cadcbaf3fec5bbe
svshyam91 <svshyam97@gmail.com>
2024-07-19 22:48:18 +0530

    Added 404 error page

--- app.js
+++ app.js
@@ -9,4 +9,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(adminRoutes);
 app.use(shopRoutes);
 
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not found</h1>');
+});
+
 app.listen(3000);

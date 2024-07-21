d37dfffc475a6f9049b5624d46dae7486755f7ab
svshyam91 <svshyam97@gmail.com>
2024-07-21 17:12:48 +0530

    changed html and css files from course project

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(rootDir, 'views', 'pageNotFound.html'));
+    res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
 });
 
 app.listen(3000);

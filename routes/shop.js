cc57cc6ce0a20d139c66b890c76aae8e7f5c5694
svshyam91 <svshyam97@gmail.com>
2024-07-27 23:02:09 +0530

    added model and controller

--- routes/shop.js
+++ routes/shop.js
@@ -1,17 +1,9 @@
 const express = require('express');
-const path = require('path');
 
-const rootDir = require('../util/path');
-const adminData = require('./admin');
+const productsController = require('../controllers/products');
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-    res.render('shop.ejs', {
-        prods: adminData.products,
-        pageTitle: 'Shop',
-        path: '/',
-    });
-});
+router.get('/', productsController.getProducts);
 
 module.exports = router;

82171d02192bdba2e14770dbf0d2cb9ce9e71ab5
svshyam91 <svshyam97@gmail.com>
2024-09-01 17:29:53 +0530

    created Product model

--- models/product.js
+++ models/product.js
@@ -1,50 +1,27 @@
-const db = require('../util/database');
+const Sequelize = require('sequelize');
 
-module.exports = class Product {
-    constructor(id, title, imageUrl, price, description) {
-        this.id = id;
-        this.title = title;
-        this.imageUrl = imageUrl;
-        this.price = price;
-        this.description = description;
-    }
+const sequelize = require('../util/database');
 
-    save() {
-        if (this.id) {
-            // Editing product
-            console.log('Editing product');
+const Product = sequelize.define('product', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true,
+    },
+    title: Sequelize.STRING,
+    price: {
+        type: Sequelize.DOUBLE,
+        allowNull: false,
+    },
+    imageUrl: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    description: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+});
 
-            return db.execute(
-                'UPDATE products SET title=?, price=?, description=?, imageUrl=? WHERE id=?',
-                [
-                    this.title,
-                    this.price,
-                    this.description,
-                    this.imageUrl,
-                    this.id,
-                ]
-            );
-        } else {
-            // Creating product
-            console.log('Adding product');
-
-            this.id = Math.random().toString();
-            return db.execute(
-                'INSERT INTO products (title, price, description, imageUrl) VALUES (?, ?, ?, ?)',
-                [this.title, this.price, this.description, this.imageUrl]
-            );
-        }
-    }
-
-    delete() {
-        return db.execute('DELETE FROM products WHERE id=?', [this.id]);
-    }
-
-    static fetchAll() {
-        return db.execute('SELECT * FROM products');
-    }
-
-    static findProductById(id) {
-        return db.execute('SELECT * FROM products WHERE id = ?', [id]);
-    }
-};
+module.exports = Product;
